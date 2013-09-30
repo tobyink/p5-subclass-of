6d57c417914c6c5b5a97bb565dc76a2be225ae61
Toby Inkster <mail@tobyinkster.co.uk>
2013-09-30 13:44:37 +0100

    s/Exporter::TypeTiny/Exporter::Tiny/

--- lib/Subclass/Of.pm
+++ lib/Subclass/Of.pm
@@ -1,10 +1,10 @@
-package Subclass::Of;
-
 use 5.008;
 use strict;
 use warnings;
 no strict qw(refs);
 no warnings qw(redefine prototype);
+
+package Subclass::Of;
 
 BEGIN {
 	$Subclass::Of::AUTHORITY = 'cpan:TOBYINK';
@@ -29,10 +29,10 @@ sub import
 	
 	return $me->install(@_, -into => scalar caller) if @_;
 	
-	require Exporter::TypeTiny;
-	our @ISA = "Exporter::TypeTiny";
+	require Exporter::Tiny;
+	our @ISA = "Exporter::Tiny";
 	@_ = $me;
-	goto \&Exporter::TypeTiny::import;
+	goto \&Exporter::Tiny::import;
 }
 
 {
